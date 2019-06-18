2ebc31f8c1a3c3cef1f92ab5f6b5e78eafca72a3
Guzix <marcinguzy.it@gmail.com>
2019-06-18 21:44:49 +0200

    Dodanie calosci

---
+++ Statystyki_OSP_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Statystyki_OSP_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Statystyki_OSP_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d91ad48-8103-42a7-912a-7641f7c40b1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Statystyki_OSP_Tests/Statystyki_OSP_Test.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Statystyki_OSP.Dane;
+using Statystyki_OSP.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statystyki_OSP_Tests
+{
+    [TestClass]
+    class Statystyki_OSP_Test
+    {
+        [TestMethod]
+        public void SaveFireman()
+        {
+            Fireman f1 = new Fireman {  Nazwisko = "Kuc", KursPodstawowy = true };
+            Fireman f2 = new Fireman {  Nazwisko = "Filipiak", KursPodstawowy = false };
+
+            DataContext.AddOrEditFiremans(f1);
+            DataContext.AddOrEditFiremans(f2);
+
+            Assert.AreEqual(DataContext.GetFiremanList().Count, 2);
+            Assert.AreEqual(DataContext.GetFiremanList()[0], f1);
+            Assert.AreEqual(DataContext.GetFiremanList()[1], f2);
+            Assert.AreEqual(DataContext.GetFiremanList()[0].KursPodstawowy, true);
+            Assert.AreEqual(DataContext.GetFiremanList()[1].KursPodstawowy, false);
+        }
+
+        public void SaveVehicle()
+        {
+            Vehicle v1 = new Vehicle { Marka = "Star", IloscMiejsc = 4 };
+            Vehicle v2 = new Vehicle { Marka = "Man", IloscMiejsc = 1 };
+
+            DataContext.AddOrEditVehicles(v1);
+            DataContext.AddOrEditVehicles(v2);
+
+            Assert.AreEqual(DataContext.GetVehiclesList().Count, 2);
+            Assert.AreEqual(DataContext.GetVehiclesList()[0], v1);
+            Assert.AreEqual(DataContext.GetVehiclesList()[1], v2);
+            Assert.AreEqual(DataContext.GetVehiclesList()[0].IloscMiejsc,4);
+            Assert.AreEqual(DataContext.GetVehiclesList()[1].IloscMiejsc,1);
+        }
+    }
+}
